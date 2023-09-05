575939299261dbc11b21333e9189241e61d0c7cb
Astien <astienth@gmail.com>
2023-09-05 03:05:18 +0200

    game configurations pages all ok

--- view/gameconfig/renderer.js
+++ view/gameconfig/renderer.js
@@ -1,4 +1,4 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, shell } = require('electron');
 const fs = require('fs');
 
 const path = require('path');
@@ -17,5 +17,15 @@ window.addEventListener('DOMContentLoaded', () => {
   const backElement = document.getElementById('back');
   backElement && backElement.addEventListener('click', () => {
     window.location.href = '../main/index.html';
+  });
+
+  const rf2link1 = document.getElementById('rf2link1');
+  rf2link1 && rf2link1.addEventListener('click', () => {
+    shell.openExternal("https://www.mediafire.com/file/s6ojcr9zrs6q9ls/rf2_sm_tools_3.7.15.1.zip/file")
+  });
+
+  const rf2link2 = document.getElementById('rf2link2');
+  rf2link2 && rf2link2.addEventListener('click', () => {
+    shell.openExternal(`file://${__dirname}/../../dll`)
   });
 });
